1c1cb1d71346c9210b3af89612f573b6e9551eab
emard <vordah@gmail.com>
2019-12-05 00:28:43 +0100

    cleanup

--- ppptun.py
+++ ppptun.py
@@ -1,6 +1,9 @@
 # micropython ESP32
 # PPP tunnel
 
+# stty -F /dev/ttyUSB0 raw
+# pppd /dev/ttyUSB0 115200 10.0.5.2:10.0.5.1 noauth local debug dump nodefaultroute nocrtscts nodetach
+
 # AUTHOR=EMARD
 # LICENSE=BSD
 
@@ -14,24 +17,14 @@ class ppptun:
   def __init__(self):
     print("PPP tunnel")
     self.uart = UART(2) # 16:RX 17:TX
+    #  on Linux:
     #  output wire wifi_gpio16, // RX input on ESP32
     #  input  wire wifi_gpio17, // TX output on ESP32
-    #  assign ftdi_rxd = wifi_gpio16 & wifi_gpio17; // echo ESP32 to FTDI, should be half duplex
     self.uart.init(baudrate=115200, bits=8, parity=None, stop=1)
     self.ppp = PPP(self.uart)
-    #self.ppp.ifconfig(('192.168.48.4', '255.255.255.0', '192.168.48.254', '8.8.8.8'))
+    #self.ppp.ifconfig(('192.168.48.4', '255.255.255.0', '192.168.48.254', '8.8.8.8')) # not needed
     self.ppp.active(True)
     self.ppp.connect()
-
-  def demo(self):
-    print("demo")
-    sleep_ms(10000)
-
-
-def demo():
-  t = ppptun()
-  t.demo()
-  del t
 
 print("usage:")
 print("p=ppptun.ppptun()")
